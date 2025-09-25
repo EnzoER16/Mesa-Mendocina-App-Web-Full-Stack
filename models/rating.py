c17542537465ffdaacca53c1c8d37c4f4d322cce
Enzo Rojas <enzo.e.rojas@gmail.com>
2025-09-25 16:51:52 -0300

    rating model updated

--- models/rating.py
+++ models/rating.py
@@ -1,29 +1,24 @@
 from config.db import db
 import uuid
+import datetime
 
 class Rating(db.Model):
     __tablename__ = 'ratings'
 
     id_rating = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    id_user = db.Column(db.String(36), nullable=False)
-    id_location = db.Column(db.String(36), nullable=False)
-    punctuation = db.Column(db.String(36), nullable=False)
-    comment = db.Column(db.String(36), nullable=False)
-    date = db.Column(db.String(36), nullable=False)
+    rate = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text)
+    date = db.Column(db.Date, default=datetime.date.today, nullable=False)
 
-    def __init__(self, id_user, id_location, punctuation, comment, date):
-        self.id_user = id_user
-        self.id_location = id_location
-        self.punctuation = punctuation
+    def __init__(self, rate, comment, date):
+        self.rate = rate
         self.comment = comment
         self.date = date
 
     def to_json(self):
         return {
             'id_rating': self.id_rating,
-            'id_user': self.id_user,
-            'id_location': self.id_location,
-            'punctuation': self.punctuation,
+            'rate': self.rate,
             'comment': self.comment,
             'date': self.date
         }
