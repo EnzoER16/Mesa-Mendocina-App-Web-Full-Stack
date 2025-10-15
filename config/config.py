a4575924a507d6ee4698c716cdf91542fab94996
LautaroVidelaa <lautymati2004@gmail.com>
2025-10-15 20:16:53 -0300

    config file updated, better organized

--- config/config.py
+++ config/config.py
@@ -4,12 +4,9 @@ import os
 # Cargar las variables de entorno desde un archivo .env
 load_dotenv()
 
-# Obtener las variables de entorno necesarias para la conexión a la base de datos
-user = os.getenv("MYSQL_USER")
-password = os.getenv("MYSQL_PASSWORD")
-host = os.getenv("MYSQL_HOST")
-database = os.getenv("MYSQL_DB")
-port = os.getenv("MYSQL_PORT", "3306")
-
-# Construir la URI de conexión a la base de datos MySQL
-DATABASE_CONNECTION_URI = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
+    # Construir la URI de conexión a la base de datos MySQL
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT','3306')}/{os.getenv('MYSQL_DATABASE')}"
+    # Desactivar el seguimiento de modificaciones
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
