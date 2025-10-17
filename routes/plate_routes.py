e6682e637dc88b93cf3f42d411e2892836bd59a5
MatyWarap <agueromatiase@gmail.com>
2025-10-17 17:33:09 -0300

    added route by id plate

--- routes/plate_routes.py
+++ routes/plate_routes.py
@@ -4,7 +4,7 @@ from models.plate import Plate
 from models.location import Location
 from routes.user_routes import token_required
 
-plates_bp = Blueprint('plates', _name_, url_prefix='/api/plates')
+plates_bp = Blueprint('plates', __name__, url_prefix='/api/plates')
 
 @plates_bp.route('/', methods=['GET'])
 def get_plates():
@@ -14,3 +14,10 @@ def get_plates():
         return jsonify({'message': 'No hay platos registrados.'}), 200
 
     return jsonify([plate.to_json() for plate in plates]), 200
+
+@plates_bp.route('/<string:id_plate>', methods=['GET'])
+def get_plate(id_plate):
+    plate = Plate.query.get(id_plate)
+    if not plate:
+        return jsonify({"error": "Plato no encontrado"}), 404
+    return jsonify(plate.to_json()), 200
