6c347f06734adefcb960729e56d4a78a22510f51
MatyWarap <agueromatiase@gmail.com>
2025-10-17 17:31:58 -0300

    added all routes plate

--- routes/plate_routes.py
+++ routes/plate_routes.py
@@ -3,3 +3,14 @@ from config.db import db
 from models.plate import Plate
 from models.location import Location
 from routes.user_routes import token_required
+
+plates_bp = Blueprint('plates', _name_, url_prefix='/api/plates')
+
+@plates_bp.route('/', methods=['GET'])
+def get_plates():
+    plates = Plate.query.all()
+
+    if not plates:
+        return jsonify({'message': 'No hay platos registrados.'}), 200
+
+    return jsonify([plate.to_json() for plate in plates]), 200
