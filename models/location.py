d217c18ee1ecc5fb6a0f38cace9cc1cc09906886
MatyWarap <agueromatiase@gmail.com>
2025-11-15 01:31:18 -0300

    schedule and price_range

--- models/location.py
+++ models/location.py
@@ -8,23 +8,24 @@ class Location(db.Model):
     name = db.Column(db.String(100), nullable=False)
     address = db.Column(db.String(150), nullable=False)
     department = db.Column(db.String(100), nullable=False)
-    schedule = db.Column(db.String(100), nullable=False)
-    price_range = db.Column(db.String(80), nullable=False)
     phone = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    image = db.Column(db.String(200), nullable=True, default="no_image.png")
 
     id_user = db.Column(db.String(36), db.ForeignKey("users.id_user"), nullable=False)
-
+    
     plates = db.relationship("Plate", backref="location", lazy=True)
     ratings = db.relationship("Rating", backref="location", lazy=True)
+    reservations = db.relationship("Reservation", backref="location", lazy=True)
 
-    def __init__(self, name, address, department, schedule, price_range, phone, id_user):
+    def __init__(self, name, address, department, phone, id_user, description=None, image="no_image.png"):
         self.name = name
         self.address = address
         self.department = department
-        self.schedule = schedule
-        self.price_range = price_range
         self.phone = phone
         self.id_user = id_user
+        self.description = description
+        self.image = image
 
     def to_json(self):
         return {
@@ -32,8 +33,8 @@ class Location(db.Model):
             'name': self.name,
             'address': self.address,
             'department': self.department,
-            'schedule': self.schedule,
-            'price_range': self.price_range,
             'phone': self.phone,
+            'description': self.description,
+            'image': self.image,
             'id_user': self.id_user
         }
