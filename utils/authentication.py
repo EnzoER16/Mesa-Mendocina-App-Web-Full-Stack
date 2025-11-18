09044ed2ed459069f68dcfb3a19433c3d788703e
Gusdeveloperr <kyubiitar.94@gmail.com>
2025-11-17 21:15:10 -0300

    authentication by logged user and role added

---
+++ utils/authentication.py
@@ -0,0 +1,27 @@
+from functools import wraps
+from flask import session, redirect, url_for, flash
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session:
+            flash('Debes iniciar sesión.', 'warning')
+            return redirect(url_for('users.user_login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+def role_required(required_role):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if 'user_id' not in session:
+                flash('Debes iniciar sesión.', 'warning')
+                return redirect(url_for('users.user_login'))
+
+            if session.get('role') != required_role:
+                flash('No tienes permisos para acceder a esta página.', 'danger')
+                return redirect(url_for('users.user_login'))
+            
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
